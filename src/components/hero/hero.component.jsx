67b4e4ca6d90fb4342e29779b55e4af454db7b80
Ruslan Bairamovi <thebairamov@gmail.com>
2020-07-20 22:49:10 +0400

    adding some animations

--- src/components/hero/hero.component.jsx
+++ src/components/hero/hero.component.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, {useEffect}from 'react'
 import './hero.style.scss'
 import Navbar from '../navbar/navbar.component'
 import Button from '../button/button.component'
 import HeroContent from '../hero-content/hero-content.component'
+import group from '../../assets/group.png'
 
 const Hero = ({text, classForPos}) => {
 
+    useEffect(() => {
+        var leftgear = document.getElementById("rotate");
+            
+        window.addEventListener("scroll", function() {
+            leftgear.style.transform = "rotate("+window.pageYOffset+"deg)";
+        });
+      });
     
 
     return (
@@ -20,7 +28,9 @@ const Hero = ({text, classForPos}) => {
             <div className="shoes-hero"></div>
             <div className="blur1"></div>
             <div className="blur2"></div>
-            <div className='circle-container'></div>
+            <div className='circle-container'>
+                <img id='rotate' src={group} alt="circle"/>
+            </div>
             <div className="newest-shoes">Newest Shoes</div>
             <Button classForPos='btn-top' text='Enjoy discounts'/>
             <Button  classForPos='btn-bottom' text='See all shoes'/>
