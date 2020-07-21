7f7513fb52740765b1fce8d41c4f24c3d6342d7c
Ruslan Bairamovi <thebairamov@gmail.com>
2020-07-21 04:00:49 +0400

    text animation css fix

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -12,10 +12,10 @@ const Navbar = () => {
         <div className='navbar'>
             <img src={navLogo} alt="nike logo"/>
               <ul className="menu">
-                <li className="menu_item">New Arrivals</li>
-                <li className="menu_item">Store</li>
-                <li className="menu_item">Videos</li>
-                <li className="menu_item">Blogs</li>
+                <li className="menu_item"><a href=""> New Arrivals </a></li>
+                <li className="menu_item"><a href=""> Store </a></li>
+                <li className="menu_item"><a href=""> Videos </a></li>
+                <li className="menu_item"><a href=""> Blogs </a></li>
             </ul> 
             <div className='cart_login'>
                 <img src={cart} alt="cart"/>
